b58a8febdf80c0896e23d26ac23f56570ed2fa17
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-12-05 22:13:34 +0100

    Format whitespace

--- src/main/java/com/hadoop/example/TokenizingMapper.java
+++ src/main/java/com/hadoop/example/TokenizingMapper.java
@@ -8,20 +8,20 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
 /**
  * Turns lines from a text file into (word, 1) tuples.
  */
-public class TokenizingMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private static final IntWritable one = new IntWritable(1);
-    
-    protected void map(LongWritable offset, Text value, Context context) 
-            throws IOException, InterruptedException {
-        
-        StringTokenizer tok = new StringTokenizer(value.toString());
-        while (tok.hasMoreTokens()) {
-            Text word = new Text(tok.nextToken());
-            context.write(word, one);
-        }
-    }
+public class TokenizingMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
+	private static final IntWritable one = new IntWritable(1);
+
+	protected void map(LongWritable offset, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		StringTokenizer tok = new StringTokenizer(value.toString());
+		while (tok.hasMoreTokens()) {
+			Text word = new Text(tok.nextToken());
+			context.write(word, one);
+		}
+	}
 }
