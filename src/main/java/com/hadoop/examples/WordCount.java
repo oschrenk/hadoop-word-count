dd1cec92217184304f44d5a4ad925635ff8a0cba
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-01-16 02:24:10 +0100

    Rename artifact to word-count

--- src/main/java/com/hadoop/examples/WordCount.java
+++ src/main/java/com/hadoop/examples/WordCount.java
@@ -27,7 +27,7 @@ public class WordCount extends Configured implements Tool {
 
 		if (args.length != 2) {
 			System.err
-					.println("Usage: hadoop jar hadoop-examples-1.0-SNAPSHOT-job.jar"
+					.println("Usage: hadoop jar word-count-1.0-SNAPSHOT-job.jar"
 							+ " [generic options] <in> <out>");
 			System.out.println();
 			ToolRunner.printGenericCommandUsage(System.err);
