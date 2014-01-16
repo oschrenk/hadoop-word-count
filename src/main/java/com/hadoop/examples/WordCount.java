c7c1498389910a8c787d064ce36c46aefaa2cd79
Oliver Schrenk <oliver.schrenk@gmail.com>
2014-01-16 02:32:49 +0100

    Use my own SumReducer instead of provided one

--- src/main/java/com/hadoop/examples/WordCount.java
+++ src/main/java/com/hadoop/examples/WordCount.java
@@ -8,7 +8,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -38,8 +37,11 @@ public class WordCount extends Configured implements Tool {
 		job.setJarByClass(getClass());
 
 		job.setMapperClass(TokenizingMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
+        job.setReducerClass(SumReducer.class);
+
+        // IntSumReducer is provided
+		//job.setCombinerClass(IntSumReducer.class);
+		//job.setReducerClass(IntSumReducer.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
